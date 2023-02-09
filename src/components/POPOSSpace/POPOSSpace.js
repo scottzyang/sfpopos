07a92222060c06ab9dfaf18fef857b9c86b55b1e
scottzyang <scottzyang@gmail.com>
2023-02-09 14:24:32 -0600

    Add additional styling to POPOSSpace

--- src/components/POPOSSpace/POPOSSpace.js
+++ src/components/POPOSSpace/POPOSSpace.js
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom'
 function POPOSSpace({ name, image, address, hours, id }) {
   return(
     <div className='POPOSSpace'>
-      <Link to={`/details/${id}`}>
+      <Link to={`/details/${id}`} className="POPOSSpace-title">
         <h1>{name}</h1>
       </Link>
-      <Link to={`/details/${id}`}>
+      <Link to={`/details/${id}`} >
         <img src={`${process.env.PUBLIC_URL}/images/${image}`} width="300" height="300" alt="50 California St." />
       </Link>
-      <div>{address}</div>
-      <div className='hours'>{hours}</div>
+      <div className='POPOSSpace-info'>      
+        <div>{address}</div>
+        <div className='hours'>{hours}</div>
+      </div>
     </div>
   )
 }
