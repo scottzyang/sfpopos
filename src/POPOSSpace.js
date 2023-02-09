2cbb0939d0faead73b39ae98144cfecdfc17e4b5
scottzyang <scottzyang@gmail.com>
2023-02-09 13:32:28 -0600

    Add more routes

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom'
 function POPOSSpace({ name, image, address, hours, id }) {
   return(
     <div className='POPOSSpace'>
-      <h1>{name}</h1>
+      <Link to={`/details/${id}`}>
+        <h1>{name}</h1>
+      </Link>
       <Link to={`/details/${id}`}>
         <img src={`${process.env.PUBLIC_URL}/images/${image}`} width="300" height="300" alt="50 California St." />
       </Link>
