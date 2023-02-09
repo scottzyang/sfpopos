da06aae6a9bb42408f6622e53a4a9bfad3ebcf38
scottzyang <scottzyang@gmail.com>
2023-02-09 14:25:18 -0600

    Move fiels

--- src/Footer.js
+++
@@ -1,7 +0,0 @@
-import './Footer.css'
-
-function Footer() {
-  return <footer>Scott Yang copyright 2023</footer>
-}
-
-export default Footer
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import POPOSList from './POPOSList';
-import About from './About';
-import POPOSDetails from './POPOSDetails';
+import POPOSList from './components/POPOSList/POPOSList';
+import About from './components/About/About';
+import POPOSDetails from './components/POPOSDetails/POPOSDetails';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
